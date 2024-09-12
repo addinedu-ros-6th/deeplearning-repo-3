ddb50c0622740eb3d177cb72843dfbaf0d291c09
YoonHee1000 <gmlcjs1018@gmail.com>
2024-09-12 11:44:38 +0900

    "relative path update"

--- Intelligence_Vehicle_AI/Perception/Lane/lane_detector_use.py
+++ Intelligence_Vehicle_AI/Perception/Lane/lane_detector_use.py
@@ -1,8 +1,8 @@
 # 사용 예시
 from lane_detector import LaneDetector
 
-detector = LaneDetector('git_ws/deeplearning-repo-3/Intelligence_Vehicle_AI/Perception/Lane/best_v8n_seg.pt',
-                         'git_ws/deeplearning-repo-3/Intelligence_Vehicle_AI/Dataset/Lane_dataset/30_only_lane_video.mp4')
+detector = LaneDetector('Intelligence_Vehicle_AI/Perception/Lane/best_v8n_seg.pt',
+                         'Intelligence_Vehicle_AI/Dataset/Lane_dataset/30_only_lane_video.mp4')
 
 for error_value, stop_line_value in detector.process_video():
     print(f"Error: {error_value}, Stop Line Flag: {stop_line_value}")
