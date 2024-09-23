9978f738179343b90d64b9e9077f5598e20a7090
Jaechang <kjc06150@gmail.com>
2024-09-23 14:51:14 +0900

    Refactor object detection algorithm and add conditional transmission of detection data

--- Intelligence_Vehicle_Service/Processor/ObstacleProcessor.py
+++ Intelligence_Vehicle_Service/Processor/ObstacleProcessor.py
@@ -8,14 +8,15 @@ import time
 import json
 
 detected_objects = {}  # 객체 정보 저장
-
 ALL_OBJECTS = ['child_deactive', 'Red_sign', 'dog', 'child', 'Blue_sign', 'stop', 'person', '50km', '50km_deactive'] # 전체 객체 정보
 detection_status_dict = {obj: False for obj in ALL_OBJECTS} # 각각의 객체 검출 상태: dictionary 형태
-detection_status_list = [False] * len(ALL_OBJECTS)  # List to store status as boolean values # 각각의 객체 검출 상태: list 형태
+REFINED_OBJECTS = ["RedSign", "ChildZone", "SpeedLimit50", "Pedestrian", "Barricade", "WildAnimal"]
+toggledSigns_dict = {objType : False for objType in REFINED_OBJECTS}
+toggledSigns_list = [False] * len(REFINED_OBJECTS)
+prev_list = []
 
 class DetectedObject:
     def __init__(self, detection_time, y_position):
-
         self.detection_time = detection_time  # 검출된 시간
         self.y_position = y_position          # 객체의 y 위치
         self.detection_count = 1              # 검출된 횟수
@@ -40,20 +41,11 @@ class ObstacleProcessor(Processor):
             if obj.detection_status and current_time > self.detection_timeout + obj.detection_time: 
                 obj.detection_count = 0
                 obj.detection_status = False
-                # if obj_name in ['person', 'dog', 'stop']:
-                #     self.notify(obj_name)
-
-    # def notify(self, object_name): 
-    #     obj = detected_objects[object_name]
-    #     if obj.detection_status:
-    #         print(f"Object {object_name}: Alert ON")
-    #     else:
-    #         print(f"Object {object_name}: Alert OFF")
 
     def execute(self, data):
-    
+
         current_time = time.time()
-        dList = data["data"]["results"] 
+        dList = data["results"]
 
         if dList != "[]":
             dList_dict = json.loads(dList) # JSON 문자열을 딕셔너리로 변환
@@ -80,62 +72,57 @@ class ObstacleProcessor(Processor):
                         if obj.detection_count > self.alert_threshold and not obj.detection_status and obj.y_position > self.yLimit_obstacle:
                             obj.detection_status = True
                             obj.detection_count = 0
+                # dictionary of entire objects
+                detection_status_dict[object_name] = detected_objects[object_name].detection_status
 
-                detection_status_dict[object_name] = detected_objects[object_name].detection_status # dictionary 
-                for i, obj_name in enumerate(ALL_OBJECTS):
-                    detection_status_list[i] = detection_status_dict[obj_name] # list
-                    
         self.check_detection_timeout(current_time) # 검출 시간 초과 여부 확인
-        # 딕셔너리와 리스트에 객체의 상태를 업데이트
 
-        
+        prev_list = toggledSigns_list.copy()
 
-        # for objName in ALL_OBJECTS: # dictionary
-        #     print(objName, " : ", detection_status_dict[objName])
-        # print("Detection status list:", detection_status_list) # list
+        if "Red_sign" in detected_objects:
+            if "Blue_sign" in detected_objects:
+                if (detected_objects["Red_sign"].detection_time > detected_objects["Blue_sign"].detection_time
+                    and detected_objects["Red_sign"].detection_status == True):
+                    toggledSigns_dict["RedSign"] = True
+                else: toggledSigns_dict["RedSign"] = False
+            else: toggledSigns_dict["RedSign"] = True
+        elif "Red_sign" not in detected_objects and "Blue_sign" in detected_objects:
+            toggledSigns_dict["RedSign"] = False
 
+        # toggledSigns_dict["RedSign"] = detection_status_dict["Red_sign"]
+        toggledSigns_dict["ChildZone"] = detection_status_dict["child"]
+        toggledSigns_dict["SpeedLimit50"] = detection_status_dict["50km"]
+        toggledSigns_dict["Pedestrian"] = detection_status_dict["person"]
+        toggledSigns_dict["Barricade"] = detection_status_dict["stop"]
+        toggledSigns_dict["WildAnimal"] = detection_status_dict["dog"]
 
+        # update list
+        for i, obj_name in enumerate(REFINED_OBJECTS):
+            toggledSigns_list[i] = toggledSigns_dict[obj_name]
+        # compare present list with previous list, print present list if they are different
+        if prev_list != toggledSigns_list:
+            print("time: ", current_time, "list : ", toggledSigns_list)
 
+## TEST ##
+import cv2
+from ultralytics import YOLO
+import json
 
+model = YOLO("./Intelligence_Vehicle_AI/Perception/Object/obstacle_n.pt")
+cap = cv2.VideoCapture("./Intelligence_Vehicle_AI/Dataset/Object_dataset/object.mp4")
+op = ObstacleProcessor()
 
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Empty video frame or completed processing")
+        break
 
+    frame = cv2.resize(frame, (720, 480))
+    results = model.track(frame, conf=0.3, imgsz=480, verbose=False)
+    obstacle_data = {
+            "results": results[0].tojson() # results 변환
+    }
+    op.execute(obstacle_data)
 
-        # if detected_objects.keys != []:
-        #     for objName in detected_objects.keys():
-        #         print(objName, " : ", detected_objects[objName].detection_status)
-
-
-### TEST ###
-# import cv2
-# from ultralytics import YOLO
-# import json
-
-# model = YOLO("./Intelligence_Vehicle_AI/Perception/Object/obstacle_n.pt")
-# cap = cv2.VideoCapture("./Intelligence_Vehicle_AI/Dataset/Object_dataset/object.mp4")
-
-# op = ObstacleProcessor()
-
-# while True:
-#     ret, frame = cap.read()
-#     if not ret:
-#         print("Empty video frame or completed processing")
-#         break
-
-#     frame = cv2.resize(frame, (720, 480))
-#     results = model.track(frame, conf=0.3, imgsz=480, verbose=False)
-#     obstacle_data = {
-#             "results": results[0].tojson() # results 변환
-#         }
-#     dList = obstacle_data["results"]
-#     if dList != "[]":
-#         dList_dict = json.loads(dList)
-#         print("--------------")
-#         for obj in dList_dict:
-#             name = obj["name"]
-#             print("name: " , name) 
-#             y1 = obj["box"]["y1"]
-#             print("y: ", y1)
-#         print("--------------")
-#     # print(dList)
-        
-# cap.release()
+cap.release()
