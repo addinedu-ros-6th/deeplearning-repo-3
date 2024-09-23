524c3327824d89580e148e3af9294882a46fc601
Jaechang <kjc06150@gmail.com>
2024-09-23 17:01:54 +0900

    Update transmission conditions for sending detection data

--- Intelligence_Vehicle_Service/Processor/ObstacleProcessor.py
+++ Intelligence_Vehicle_Service/Processor/ObstacleProcessor.py
@@ -9,11 +9,10 @@ import json
 
 detected_objects = {}  # 객체 정보 저장
 ALL_OBJECTS = ['child_deactive', 'Red_sign', 'dog', 'child', 'Blue_sign', 'stop', 'person', '50km', '50km_deactive'] # 전체 객체 정보
-detection_status_dict = {obj: False for obj in ALL_OBJECTS} # 각각의 객체 검출 상태: dictionary 형태
 REFINED_OBJECTS = ["RedSign", "ChildZone", "SpeedLimit50", "Pedestrian", "Barricade", "WildAnimal"]
 toggledSigns_dict = {objType : False for objType in REFINED_OBJECTS}
 toggledSigns_list = [False] * len(REFINED_OBJECTS)
-prev_list = []
+toggledSigns_prev = []
 
 class DetectedObject:
     def __init__(self, detection_time, y_position):
@@ -30,22 +29,23 @@ class DetectedObject:
 class ObstacleProcessor(Processor):
     def __init__(self):
         self.alert_threshold = 20 #반복 검출 조건(횟수)
-        self.detection_timeout = 1 #검출 해제 조건(시간)
-        self.yLimit_signs = 120 #표지판 y조건
-        self.yLimit_obstacle = 240 #장애물 y조건
+        self.detection_timeout = 3 #검출 해제 조건(시간)
+        self.yLimit_signs = 0 #표지판 y조건
+        self.yLimit_obstacle = 0 #장애물 y조건
 
     def check_detection_timeout(self, current_time): # 검출 시간이 초과되었는지 확인하고 객체의 상태를 업데이트하는 메소드
         for obj_name in list(detected_objects.keys()):
-            obj = detected_objects[obj_name]
-            # 현재 시간이 검출 해제 시간보다 크면 객체의 검출 상태를 해제하고 검출 횟수를 초기화
-            if obj.detection_status and current_time > self.detection_timeout + obj.detection_time: 
-                obj.detection_count = 0
-                obj.detection_status = False
+            if obj_name in ['person', 'dog', 'stop']:
+                obj = detected_objects[obj_name]
+                if obj.detection_status and current_time > self.detection_timeout + obj.detection_time:
+                    obj.detection_count = 0
+                    obj.detection_status = False
 
     def execute(self, data):
 
         current_time = time.time()
         dList = data["results"]
+        toggledSigns_prev = toggledSigns_list.copy()
 
         if dList != "[]":
             dList_dict = json.loads(dList) # JSON 문자열을 딕셔너리로 변환
@@ -64,65 +64,72 @@ class ObstacleProcessor(Processor):
                         if obj.detection_count > self.alert_threshold and not obj.detection_status:
                             obj.detection_status = True
                             obj.detection_count = 0
-                    elif object_name in ['child_deactivate', 'child', '50km', '50km_deactivate']:
-                        if obj.detection_count > self.alert_threshold and not obj.detection_status and obj.y_position > self.yLimit_signs:
+                            match object_name:
+                                case "Red_sign":
+                                    toggledSigns_dict["RedSign"] = True
+                                    if "Blue_sign" in detected_objects.keys():
+                                        detected_objects["Blue_sign"].detection_status = False
+                                case "Blue_sign":
+                                    toggledSigns_dict["RedSign"] = False
+                                    if "Red_sign" in detected_objects.keys():
+                                        detected_objects["Red_sign"].detection_status = False
+                    elif object_name in ['child_deactive', 'child', '50km', '50km_deactive']:
+                        if obj.detection_count > self.alert_threshold and (not obj.detection_status) and obj.y_position > self.yLimit_signs:
                             obj.detection_status = True
                             obj.detection_count = 0
+                            match object_name:
+                                case "child" : toggledSigns_dict["ChildZone"] = True;
+                                case "child_deactive": toggledSigns_dict["ChildZone"] = False
+                                case "50km": toggledSigns_dict["SpeedLimit50"] = True
+                                case "50km_deactive": toggledSigns_dict["SpeedLimit50"] = False
                     elif object_name in ['person', 'dog', 'stop']:
-                        if obj.detection_count > self.alert_threshold and not obj.detection_status and obj.y_position > self.yLimit_obstacle:
+                        if obj.detection_count > self.alert_threshold and (not obj.detection_status) and obj.y_position > self.yLimit_obstacle:
                             obj.detection_status = True
                             obj.detection_count = 0
-                # dictionary of entire objects
-                detection_status_dict[object_name] = detected_objects[object_name].detection_status
+                            print("obj name : ", object_name)
+
+        for obstacle in ["person", "stop", "dog"]:
+            if obstacle not in list(detected_objects.keys()):
+                continue
+            obj = detected_objects[obstacle]
+            match obstacle:
+                case "person" : toggledSigns_dict["Pedestrian"] = obj.detection_status
+                case "stop" : toggledSigns_dict["Barricade"] = obj.detection_status
+                case "dog" : toggledSigns_dict["WildAnimal"] = obj.detection_status
 
         self.check_detection_timeout(current_time) # 검출 시간 초과 여부 확인
-
-        prev_list = toggledSigns_list.copy()
-
-        if "Red_sign" in detected_objects:
-            if "Blue_sign" in detected_objects:
-                if (detected_objects["Red_sign"].detection_time > detected_objects["Blue_sign"].detection_time
-                    and detected_objects["Red_sign"].detection_status == True):
-                    toggledSigns_dict["RedSign"] = True
-                else: toggledSigns_dict["RedSign"] = False
-            else: toggledSigns_dict["RedSign"] = True
-        elif "Red_sign" not in detected_objects and "Blue_sign" in detected_objects:
-            toggledSigns_dict["RedSign"] = False
-
-        # toggledSigns_dict["RedSign"] = detection_status_dict["Red_sign"]
-        toggledSigns_dict["ChildZone"] = detection_status_dict["child"]
-        toggledSigns_dict["SpeedLimit50"] = detection_status_dict["50km"]
-        toggledSigns_dict["Pedestrian"] = detection_status_dict["person"]
-        toggledSigns_dict["Barricade"] = detection_status_dict["stop"]
-        toggledSigns_dict["WildAnimal"] = detection_status_dict["dog"]
-
         # update list
         for i, obj_name in enumerate(REFINED_OBJECTS):
             toggledSigns_list[i] = toggledSigns_dict[obj_name]
         # compare present list with previous list, print present list if they are different
-        if prev_list != toggledSigns_list:
+        if toggledSigns_prev != toggledSigns_list:
             print("time: ", current_time, "list : ", toggledSigns_list)
 
 ## TEST ##
-import cv2
-from ultralytics import YOLO
-import json
-
-model = YOLO("./Intelligence_Vehicle_AI/Perception/Object/obstacle_n.pt")
-cap = cv2.VideoCapture("./Intelligence_Vehicle_AI/Dataset/Object_dataset/object.mp4")
-op = ObstacleProcessor()
-
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("Empty video frame or completed processing")
-        break
-
-    frame = cv2.resize(frame, (720, 480))
-    results = model.track(frame, conf=0.3, imgsz=480, verbose=False)
-    obstacle_data = {
-            "results": results[0].tojson() # results 변환
-    }
-    op.execute(obstacle_data)
-
-cap.release()
+# import cv2
+# from ultralytics import YOLO
+# import json
+#
+# model = YOLO("./Intelligence_Vehicle_AI/Perception/Object/obstacle_n.pt")
+# cap = cv2.VideoCapture("./Intelligence_Vehicle_AI/Dataset/Object_dataset/object.mp4")
+# op = ObstacleProcessor()
+#
+# while True:
+#     ret, frame = cap.read()
+#     if not ret:
+#         print("Empty video frame or completed processing")
+#         break
+#
+#     frame = cv2.resize(frame, (720, 480))
+#     results = model.track(frame, conf=0.3, imgsz=480, verbose=False)
+#     obstacle_data = {
+#             "results": results[0].tojson() # results 변환
+#     }
+#     op.execute(obstacle_data)
+#     cv2.imshow("Live Camera", results[0].plot())
+#
+#     if cv2.waitKey(1) == ord('q'):
+#         break
+#
+# cap.release()
+# cv2.destroyAllWindows()
